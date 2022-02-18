0d90587698aaeb8926c7854e38c3ca85f3502170
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-02-18 21:58:03 +0200

    Update dk-parking.sql

--- dk-parking.sql
+++ dk-parking.sql
@@ -1,2 +1,2 @@
+ALTER TABLE `player_vehicles` CHANGE COLUMN `garage` `street` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci' AFTER `fakeplate`;
 ALTER TABLE `player_vehicles` ADD COLUMN `coords` TEXT NULL DEFAULT NULL
-ALTER TABLE `player_vehicles` CHANGE COLUMN `garage` `street` varchar(50) NULL DEFAULT NULL
