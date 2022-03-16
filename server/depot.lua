0705c1d9f946e2a421a2a7c3719640b19a529f5a
Roberts Bauers <86129179+darkets@users.noreply.github.com>
2022-03-16 16:42:23 +0200

    chore(server/depot): update sql exports

--- server/depot.lua
+++ server/depot.lua
@@ -1,6 +1,4 @@
 local depotVehicles = {}
-local cid = nil
-local locales = Config.Phrases
 
 QBCore = exports['qb-core']:GetCoreObject()
 
@@ -19,26 +17,25 @@ RegisterNetEvent('qb-garage:server:PayDepotPrice', function(vehicle, key)
     local Player = QBCore.Functions.GetPlayer(src)
     local cashBalance = Player.PlayerData.money["cash"]
     local bankBalance = Player.PlayerData.money["bank"]
-    exports.oxmysql:execute('SELECT * FROM player_vehicles WHERE plate = ?', {vehicle.plate}, function(result)
-        if result[1] then
-            if cashBalance >= result[1].depotprice then
-                Player.Functions.RemoveMoney("cash", result[1].depotprice, "paid-depot")
-                TriggerClientEvent("depot:client:takeOut", src, vehicle, key)
-            elseif bankBalance >= result[1].depotprice then
-                Player.Functions.RemoveMoney("bank", result[1].depotprice, "paid-depot")
-                TriggerClientEvent("depot:client:takeOut", src, vehicle, key)
-            else
-                TriggerClientEvent('QBCore:Notify', 'Not enough money!', 'error')
-            end
+    local result = MySQL.query.await('SELECT * FROM player_vehicles WHERE plate = ?', {vehicle.plate})
+    if result[1] then
+        if cashBalance >= result[1].depotprice then
+            Player.Functions.RemoveMoney("cash", result[1].depotprice, "paid-depot")
+            TriggerClientEvent("depot:client:takeOut", src, vehicle, key)
+        elseif bankBalance >= result[1].depotprice then
+            Player.Functions.RemoveMoney("bank", result[1].depotprice, "paid-depot")
+            TriggerClientEvent("depot:client:takeOut", src, vehicle, key)
+        else
+            TriggerClientEvent('QBCore:Notify', 'Not enough money!', 'error')
         end
-    end)
+    end
     depotVehicles[key] = nil
 end)
 
 AddEventHandler("onResourceStart", function(resourceName)
-	if ("cx-parking" == resourceName) then
-        Citizen.Wait(1000)
-        local result = exports.oxmysql:executeSync('SELECT * FROM player_vehicles WHERE NOT state = ?', {1})
+    if ("cx-parking" == resourceName) then
+        Wait(1000)
+        local result = MySQL.query.await('SELECT * FROM player_vehicles WHERE NOT state = ?', {1})
         for k, v in pairs(result) do
             depotVehicles[#depotVehicles + 1] = v
         end
